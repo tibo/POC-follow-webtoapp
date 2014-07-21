09ddf54fb9160340d5c3efc04459f7335169dedb
Thibaut LE LEVIER <thibaut@lelevier.fr>
2014-07-21 17:18:27 +0200

    prepare redirect to app

--- web.py
+++ web.py
@@ -26,5 +26,11 @@ def logout():
 
 app.secret_key = 'YouDontKnowMySecret'
 
+@app.route('/redirectToApp')
+def redirect_to_app():
+	if 'login' in session:
+		return redirect('followapp://login/%s' % session['login'])
+	return redirect(url_for('login'))
+
 if __name__ == "__main__":
     app.run(debug=True)
