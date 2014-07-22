095fc758529d1ebf384acab1ef18f80bbf113922
Thibaut LE LEVIER <thibaut@lelevier.fr>
2014-07-22 16:28:24 +0200

    follow api call with unique token + ip check

--- Readme.md
+++ Readme.md
@@ -18,8 +18,3 @@ Live version on heroku : [http://follow-web-to-app.herokuapp.com/](http://follow
 ## Proof of concept
 
 This is just a proof of concept/test for a web to app session workflow and also a first time for Flask and me.
-
-### todo :
-- security : basic webform security
-- security : sso token valid only for few minutes
-- security : call the client app with the token + call a webservice with the token from the app to get the full session
--- web.py
+++ web.py
@@ -1,5 +1,5 @@
 import os, md5
-from flask import Flask, session, redirect, url_for, request, render_template
+from flask import Flask, session, redirect, url_for, request, render_template, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_folder='static', static_url_path='')
@@ -12,6 +12,8 @@ class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	login = db.Column(db.String(255), unique=True)
 	password = db.Column(db.String(255))
+	follow_key = db.Column(db.String(255))
+	follow_source_ip = db.Column(db.String(255))
 
 	def __init__(self, login, password):
 		self.login = login
@@ -26,10 +28,10 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        result = User.query.filter(User.login == request.form['login'], User.password == md5.new(request.form['password']).hexdigest()).all()
+        results = User.query.filter(User.login == request.form['login'], User.password == md5.new(request.form['password']).hexdigest()).all()
 
-        if len(result) == 1:
-        	session['login'] = result[0].login
+        if len(results) == 1:
+        	session['login'] = results[0].login
         	return redirect(url_for('index'))
 
         return redirect(url_for('login'))
@@ -56,8 +58,31 @@ app.secret_key = 'YouDontKnowMySecret'
 @app.route('/redirectToApp')
 def redirect_to_app():
 	if 'login' in session:
-		return redirect('followapp://login/%s' % session['login'])
+
+		user = User.query.filter(User.login == session['login']).first()
+		user.follow_key = os.urandom(10).encode('hex')
+		user.follow_source_ip = request.remote_addr
+
+		db.session.commit()
+
+		return redirect('followapp://%s' % user.follow_key)
 	return redirect(url_for('login'))
+
+@app.route('/getSession.json')
+def get_session():
+
+	results = User.query.filter(User.follow_key == request.args.get('follow_key'), User.follow_source_ip == request.remote_addr).all()
+
+	if len(results) == 1:
+		user = results[0]
+		user.follow_key = None
+		user.follow_source_ip = None
+
+		db.session.commit()
+
+		return jsonify({'login': user.login}), 200
+
+	return jsonify({'error': 'not authorized'}), 401
 
 if __name__ == "__main__":
     app.run(debug=True)
